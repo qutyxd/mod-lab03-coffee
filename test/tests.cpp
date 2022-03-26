be7cbc87411be84ec5e6c27116529c305dfc4ce0
qutyxd <100722945+qutyxd@users.noreply.github.com>
2022-03-26 14:13:56 +0300

    Updated tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,31 @@
-// Copyright 2022 GHA Test Team
-
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(task, test1)
+{
+    Automata machine = Automata();
+    machine.on();
+    machine.coin(13);
+    machine.choice(2);
+    States result = machine.getState();
+    EXPECT_EQ(WAIT, result);
+}
+
+TEST(task, test2)
+{
+    Automata machine = Automata();
+    machine.on();
+    machine.choice(2);
+    States result = machine.getState();
+    EXPECT_EQ(WAIT, result);
+}
+
+TEST(task, test3)
+{
+    Automata machine = Automata();
+    machine.on();
+    machine.coin(20);
+    machine.choice(1);
+    int result = machine.finish();
+    EXPECT_EQ(10, result);
+}
