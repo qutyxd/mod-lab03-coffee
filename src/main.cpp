7d55838c65fba52606fbf6d6eb9b97fb9ade70dc
qutyxd <100722945+qutyxd@users.noreply.github.com>
2022-03-26 17:25:05 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include "Automata.h"
 
-using namespace std;
+using std::string;
 
-int main()
-{
+int main() {
     Automata machine = Automata();
-
     machine.on();
-    machine.etMenu();
+    machine.getMenu();
     machine.coin(20);
     machine.choice(1);
     machine.finish();
@@ -17,6 +15,5 @@ int main()
     machine.choice(3);
     machine.finish();
     machine.off();
-
     return 0;
 }
